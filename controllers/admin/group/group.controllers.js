6f0ee119d998d3cb4b4389d1304918473ffdd4d3
Vishal Verma <assistantjunior@earninghandle.com>
2025-02-24 16:47:44 +0530

    make group controllers

--- controllers/admin/group/group.controllers.js
+++ controllers/admin/group/group.controllers.js
@@ -29,4 +29,79 @@ const addGroup = async (req, res) => {
 
 }
 
-module.exports = {addGroup};
+const getGroupInfo = async (req, res) => {
+
+    try {
+        const { groupName } = req.body;
+        if (!groupName) {
+            return res.status(404).json({ success: false, error: "Group name is compulsary" });
+        }
+
+        const group = await Group.findOne({ groupName });
+
+        if (!group) {
+            return res.status(404).json({ success: false, error: "Group not found" });
+        }
+
+        const groupMemberCount = await Users.countDocuments({ groups: groupName });
+
+        if (!groupMemberCount) {
+            return res.status(404).json({ success: false, error: "No group member found" });
+        }
+
+        group.totalUsers = groupMemberCount;
+        await group.save();
+
+        return res.status(200).json({ success: true, group });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: error.message });
+    }
+
+}
+
+const addUserGroup = async (req, res) => {
+
+    try {
+        const userId = req.params.userId;
+        const { groupName } = req.body;
+
+        const user = await Users.findById(userId);
+
+        if (!user) {
+            return res.status(404).json({ success: false, error: "User not found" });
+        }
+
+        user.groups = groupName;
+        await user.save();
+
+        return res.status(200).json({ success: true, user });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: error.message });
+    }
+
+}
+
+const deleteGroup = async (req, res) => {
+    try {
+        const groupId = req.params.groupId;
+
+        if (!groupId) {
+            return res.status(404).json({ success: false, error: "Group id not found." });
+        }
+
+        const deletedgroup = await Group.findByIdAndDelete(groupId);
+
+        if (!deletedgroup) {
+            return res.status(404).json({ success: false, error: "Group not found." });
+        }
+
+        return res.status(200).json({ success: true, deletedgroup });
+    } catch (error) {
+        return res.status(500).json({ success: false, error: error.message });
+    }
+
+}
+
+const setDefaultGroup = () => { }
+
+module.exports = { addGroup, getGroupInfo, deleteGroup };
--- routes/admin/group/group.routes.js
+++ routes/admin/group/group.routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const verifyJWT = require('../../../middleware/authMiddleware.js');
-const {addGroup} = require('../../../controllers/admin/group/group.controllers.js');
+const {addGroup, getGroupInfo} = require('../../../controllers/admin/group/group.controllers.js');
 const {upload} = require('../../../utils/multer.js')
 
 const router = express.Router();
 
 // add group
 router.post("/addGroup", verifyJWT, upload.single("groupImage"), addGroup);
+
+// get group information
+router.get("/getGroupInfo", verifyJWT, getGroupInfo);
 
 
 
