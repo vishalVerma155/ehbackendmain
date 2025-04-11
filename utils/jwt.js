93be33fb67fb3eb41c3153cad12147ba7f7730a2
Vishal Verma <assistantjunior@earninghandle.com>
2025-04-11 15:10:38 +0530

    increase jwt timing

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt  = require('jsonwebtoken');
 
 const generateJWT = (user) =>{
-    return jwt.sign(user, process.env.JWT_SECURITY_KEY, {expiresIn : "1h"});
+    return jwt.sign(user, process.env.JWT_SECURITY_KEY, {expiresIn : "4h"});
 }
 
 module.exports = generateJWT;
